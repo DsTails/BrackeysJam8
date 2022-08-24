242af973a66248b7eaf67c1a0fc17db127d9412d
Tom <u1909810@uel.ac.uk>
2022-08-24 14:26:24 +0100

    Major changes (Throwing objects, throwing line, adjustments to player type, etc.)

---
+++ Brackeys_Jam_8_You_Are_Not_Alone/Assets/Scripts/Player/Object.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour, IObjectGrabInterface
+{
+    public bool isBeingGrabbed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ObjectGrabbed()
+    {
+        isBeingGrabbed = true;
+    }
+
+    public void ResetObjectGrab()
+    {
+        isBeingGrabbed = false;
+    }
+}
--- Brackeys_Jam_8_You_Are_Not_Alone/Assets/Scripts/Player/PlayerMovement.cs
+++ Brackeys_Jam_8_You_Are_Not_Alone/Assets/Scripts/Player/PlayerMovement.cs
@@ -16,10 +16,19 @@ public class PlayerMovement : MonoBehaviour
 
     //Declaring Movement Variables
     [SerializeField]
-    private float runSpeed = 40f;
+    public float runSpeed = 40f;
 
     //Declaring Photon View variable
     PhotonView view;
+
+    //Wall Jumping
+    public bool canWallJump;
+    public bool wallSliding;
+    public float wallJumpTime;
+    public float wallSlideSpeed;
+    public float wallDistance;
+    RaycastHit2D WallCheckHit;
+    float JumpTime;
 
     // Start is called before the first frame update
     void Start()
@@ -40,8 +49,41 @@ public class PlayerMovement : MonoBehaviour
 
     void FixedUpdate()
     {
+        if(GetComponent<PlayerType>().isBeingGrabbed || GetComponent<PlayerType>().isBeingThrown)
+        {
+            return;
+        }
+
+        if(controller.m_Grounded && GetComponent<PlayerType>().isBeingThrown)
+        {
+            GetComponent<PlayerType>().isBeingThrown = false;
+        }
+
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jumping);
         jumping = false;
+
+        //Wall jump logic
+        if (canWallJump)
+        {
+            WallCheckHit = Physics2D.Raycast(transform.position, new Vector2(wallDistance * (controller.m_FacingRight ? 1 : -1), 0f), wallDistance, controller.m_WhatIsGround);
+            Debug.DrawRay(transform.position, new Vector2(wallDistance * (controller.m_FacingRight ? 1 : -1), 0f), Color.blue);
+
+            if (WallCheckHit && !controller.m_Grounded && Input.GetAxisRaw("Horizontal") != 0)
+            {
+                wallSliding = true;
+                JumpTime = Time.time + wallJumpTime;
+
+            } else if(JumpTime < Time.time)
+            {
+                wallSliding = false;
+            }
+
+            if (wallSliding)
+            {
+                controller.m_Rigidbody2D.velocity = new Vector2(controller.m_Rigidbody2D.velocity.x, Mathf.Clamp(controller.m_Rigidbody2D.velocity.y, wallSlideSpeed, float.MaxValue));
+            }
+        }
+
     }
 
     #region ~Movement~
